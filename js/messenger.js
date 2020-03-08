273e325d54dd6c17920c77adc7beb6e4b81c64df
longnd1003 <51944092+longnd1003@users.noreply.github.com>
2020-03-08 11:56:45 +0700

    Update messenger.js
    Update voice chooser

--- js/messenger.js
+++ js/messenger.js
@@ -1,23 +1,39 @@
-function sendMsg() {
-	var inputMsg = $("#input-msg").val();
-	$('div#new-msg').removeAttr('id');
-	$("#msg-zone").append("<div id=\"new-msg\" class=\"media w-50 ml-auto mb-3\"><div class=\"media-body\"><div class=\"bg-primary rounded py-3 px-3 mb-2\"><p class=\"text-small mb-0 text-white\">" + inputMsg + "</p></div><p class=\"small text-muted\">12:00 PM | Aug 13</p></div></div>");
-	document.getElementById("input-msg").value = "";
+$(document).ready(function () {
+    var welcomeMsg = "Xin chào các bạn!";
+    botCreateMsg(welcomeMsg);
+	botCreateAudio(welcomeMsg);
+});
+
+function getVoice() {
+	var url_string = window.location.href;
+	var url = new URL(url_string);
+	var voice = url.searchParams.get("voice");
+	return voice;
 }
 
-function getBotMsg() {
-	var text = 'input=' + $("#bot-msg").text();
-	$.ajax({
-		url: 'tts.php',
-		type: 'POST',
-		data: text,
-		async: true,
-		success: function (data) {
-			$("#tts-audio").html(data)
-		},
-	});
+function sendMsg() {
+    var inputMsg = $("#input-msg").val();
+	var today = new Date();
+	var time = today.getHours() + ":" + today.getMinutes();
+    $("#msg-zone").append("<div class=\"media w-50 ml-auto mb-3\"><div class=\"media-body\"><div class=\"bg-primary rounded py-3 px-3 mb-2\"><p class=\"text-small mb-0 text-white\">" + inputMsg + "</p></div><p class=\"small text-muted\">Sent at " + time + "</p></div></div>");
+    document.getElementById("input-msg").value = "";
+}
+
+function botCreateAudio(inputMsg) {
+    var text = 'input=' + inputMsg + '&voice=' + getVoice();
+    $.ajax({
+        url: 'tts.php',
+        type: 'POST',
+        data: text,
+        async: true,
+        success: function (data) {
+            $("#tts-audio").html(data)
+        },
+    });
 }
 
 function botCreateMsg(msg) {
-	
+	var today = new Date();
+	var time = today.getHours() + ":" + today.getMinutes();
+    $("#msg-zone").append("<div class=\"media w-50 mb-3\"><img src=\"img/restaurant.png\" alt=\"user\" width=\"50\" class=\"rounded-circle\"><div class=\"media-body ml-3\"><div class=\"bg-light rounded py-3 px-3 mb-2\"><p id=\"bot-msg\" class=\"text-small mb-0 text-muted\">" + msg + "</p></div><p class=\"small text-muted\">Received at " + time + "</p></div></div>");
 }
