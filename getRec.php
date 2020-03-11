30175a62b41a6ec3c71406c06a79ad16c29736cd
longnd1003 <51944092+longnd1003@users.noreply.github.com>
2020-03-11 16:18:07 +0700

    Delete getRec.php

--- getRec.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$input = $_FILES['audio']['tmp_name'];
-	$output = date("dmYhis").'.wav';
-	if (move_uploaded_file($input, $output)) {
-		exit('Audio file Uploaded');
-	}
-	exit(print_r($_FILES));
-?>
